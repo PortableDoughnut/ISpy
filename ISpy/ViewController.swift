1daf6d17f61a57ec3fd41b6954ca1cdee4c14930
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-14 09:53:30 -0600

    Completed Project

--- ISpy/ViewController.swift
+++ ISpy/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+	@IBOutlet weak var gwenDrawingImage: UIImageView!
+	@IBOutlet weak var scrollView: UIScrollView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		scrollView.delegate = self
 	}
 
+	func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+		return gwenDrawingImage
+	}
+	
+	func updateZoomFor(size: CGSize) {
+		let widthScale: CGFloat = size.width / gwenDrawingImage.bounds.width
+		let heightScale: CGFloat = size.height / gwenDrawingImage.bounds.height
+		let scale: CGFloat = min(widthScale, heightScale)
+		
+		scrollView.minimumZoomScale = scale
+		scrollView.zoomScale = scale
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		updateZoomFor(size: view.bounds.size)
+	}
 
 }
 

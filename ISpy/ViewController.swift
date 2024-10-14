e048d89e4ca6b6b8c9bd48727b546ea95bb51e96
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-14 09:07:39 -0600

    Initial Commit

---
+++ ISpy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Gwen Thelin on 10/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ ISpyTests/ISpyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ISpyTests.swift
+//  ISpyTests
+//
+//  Created by Gwen Thelin on 10/14/24.
+//
+
+import Testing
+@testable import ISpy
+
+struct ISpyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
